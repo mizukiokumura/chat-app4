09be3e8ee0d9f647451aa84d577cf561fc8710be
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-04-17 10:42:57 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
+# 問題３
+# 新規チャットルームの作成で動くアクションは「new」と「create」のみなので、上記のようなコードになる。
