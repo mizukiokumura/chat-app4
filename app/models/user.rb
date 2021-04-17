4fed253ac1a2d4458e6fc60678f00fb612c82045
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-04-17 10:31:49 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
 
 end
 
-問題１１
-Userモデルに、validates :name, presence: trueを追記します。
-nameカラムに、presence: trueを設けることで、からの場合はDBに保存しないという
-バリデーションを設定しています。
-つまり、ユーザー登録時に「name」を空欄にして登録しようとすると、エラーが発生する。
+# 問題１１
+# Userモデルに、validates :name, presence: trueを追記します。
+# nameカラムに、presence: trueを設けることで、からの場合はDBに保存しないという
+# バリデーションを設定しています。
+# つまり、ユーザー登録時に「name」を空欄にして登録しようとすると、エラーが発生する。
