ce30b25885120fac93a46ec247c108a9af83194a
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-04-21 11:43:01 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
+      @messages = @room.messages.includes(:user)
       render :new
     end
   end
@@ -30,3 +32,11 @@ end
 # has_manyのアソシエーションを組んでいれば、親モデルのインスタンス.子モデルの複数形(小文字).new
 # という記述方法で、親モデルのインスタンスに紐づく子モデルのインスタンスを生成できる。
 
+# 問題７
+# チャットルームに紐づいている全てのメッセージ(@room.messages)を@messagesとして定義。
+# この場合、メッセージに紐づくユーザー情報の取得に、メッセージの数と同じ回数のアクセスが必要になるので、
+# N+1問題が発生する
+# その場合は、includesを使用して解消し、全てのメッセージ情報に紐づくユーザー情報を、includes(:user)と記述することにより、
+# ユーザー情報を１度のアクセスでまとめて取得が可能になる。
+# この時に投稿に失敗した時の処理にも、同様に@messagesを定義しておくことでm
+# renderを用いて、投稿に失敗した@messageの情報を保持しつつindex.html.erbを参照する事ができる。
